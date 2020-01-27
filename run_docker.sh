43984495e3a8510cf5820dcf157556244457fa75
omer aiman <ew18@protonmail.com>
2020-01-27 20:30:38 +0300

    adding project files

---
+++ run_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+
+docker build -t flaskapp:1.0 .
+
+# Step 2: 
+# List docker images
+
+docker ps
+
+# Step 3: 
+# Run flask app
+
+docker run -p 8081:80 -t flaskapp:1.0
